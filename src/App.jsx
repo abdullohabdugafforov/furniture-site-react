01a5a2e7bc0dc62ed42d5513f475c857dce6a69d
abdulloh <abdulloh@MacBook-Air-abdulloh.local>
2025-12-05 10:38:48 +0500

    nav & home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import {Link} from 'react-router-dom'
+import Left from '../public/<.png'
+import Right from '../public/>.png'
+import Line from '../public/Line.png'
+
 
 export default function App() {
 
   return (
-    <h1>Furniture home page</h1>
+    <header className="home container">
+      <p>WE ARE <img src={Line} alt="" /></p>
+      <h1>
+        <span>Modern</span> Furniture <br /> Center
+      </h1>
+      <p>We help you to create organize your room to be more cozy, design by professional interior designer</p>
+      <Link className='btn-primary' to='/products'>See Our Product</Link>
+
+      <div className='home-controls'>
+        <span><img src={Left} alt="" /></span>
+        <span><img src={Right} alt="" /></span>
+      </div>
+    </header>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,23 +1,25 @@
 import { Link } from 'react-router-dom'
+import Bag from '../public/bag.png'
+import Logo from '../public/FURNITURE.png'
 
 export default function Navbar() {
 
     return (
-        <>
+        <nav className='container'>
             <ul>
                 <li>
-                    <Link to='/'>Logo new</Link>
+                    <Link to='/'><img src={Logo} alt="" /></Link>
                 </li>
                 <li>
                     <ul>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/aboutus'>About Us</Link></li>
                         <li><Link to='/products'>Products</Link></li>
-                        <li><Link to='/contacts'>Contacts</Link></li>
-                        <li><Link to='/contacts'>Contacts</Link></li>
+                        <li><Link to='/products'><img src={Bag} alt="" /></Link></li>
+                        <li><Link className='btn-nav' to='/contacts'>Contact Us</Link></li>
                     </ul>
                 </li>
             </ul>
-        </>
+        </nav>
     )
 }
